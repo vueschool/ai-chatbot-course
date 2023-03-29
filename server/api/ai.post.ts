ed99c661719957fb6275d8487b3c9363a8ba1cb8
Daniel Kelly <me@danielkelly.io>
2023-03-28 19:11:11 -0500

    remove unneeded util

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,17 +1,23 @@
-import { getOpenAI } from "@/server/utils";
 import * as trainings from "@/training";
+import { Configuration, OpenAIApi } from "openai";
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
-  const openai = getOpenAI();
+  // Setup open AI
+  const { OPENAI_API_KEY } = useRuntimeConfig();
+  const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
+  // send request to Chat Model
   try {
     if (!Object.keys(trainings).includes(`${body.training}Training`)) {
       throw new Error(`Training ${body.training} does not exist`);
     }
 
     const { data } = await openai.createChatCompletion({
-      model: body.model || "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo",
       temperature: body.temperature || 1,
       messages: body.messages || [],
       // @ts-expect-error checking above if training exists
--- server/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-export function getOpenAI() {
-  const { OPENAI_API_KEY } = useRuntimeConfig();
-  const configuration = new Configuration({
-    apiKey: OPENAI_API_KEY,
-  });
-  return new OpenAIApi(configuration);
-}
