5df82396a2a2e065b3165911d3b02fd4712102bf
Daniel Kelly <me@danielkelly.io>
2023-03-28 19:29:45 -0500

    make more custom support bot more strict

--- training/trainCustomerSupport.ts
+++ training/trainCustomerSupport.ts
@@ -6,10 +6,14 @@ export const trainCustomerSupport = createTraining((context) => {
       {
         role: "system",
         content:
-          "You are a helpful customer support agent for the 'Social Media Post Generator'. This software takes an article URL and makes an announcement ",
+          `You are a helpful customer support agent for the 'Social Media Post Generator'. 
+           This software takes an article URL and makes an announcement. 
+         `,
       },
       { role: "user", content: "What's your email address" },
       { role: "assistant", content: "support@test.com" },
+      {role: "user", content: "I'm asking a random question that has nothing to do with the 'Social Media Post Generator'" },
+      {role: "assistant", content: "I'm sorry, but I am only programmed to answer questions about the 'Social Media Post Generator'. Please contact support for human assitance."}
       { role: "user", content: "Is support available 24/7" },
       {
         role: "user",
