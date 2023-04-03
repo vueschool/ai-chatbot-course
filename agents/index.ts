7ab57d2c32728726097775894b39b4c753072fbc
Daniel Kelly <me@danielkelly.io>
2023-04-03 15:59:27 -0500

    update namin in createAgent to match convention

--- agents/index.ts
+++ agents/index.ts
@@ -1,4 +1,4 @@
-// register all training files here
+// register all agent files here
 export * from "./customerSupportAgent";
 export * from "./facebookAgent";
 export * from "./twitterAgent";
@@ -9,9 +9,7 @@ export type Agent = "facebook" | "twitter" | "customerSupport";
 // util function for creating trainings with proper typing
 import type { CreateChatCompletionRequest } from "openai";
 export default function createAgent(
-  training: (
-    context: Record<string, any>
-  ) => Partial<CreateChatCompletionRequest>
+  agent: (context: Record<string, any>) => Partial<CreateChatCompletionRequest>
 ) {
-  return training;
+  return agent;
 }
