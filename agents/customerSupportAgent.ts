8e39b76274c462e8081149875e89211f8caa0f7e
Daniel Kelly <me@danielkelly.io>
2023-04-12 15:52:23 -0500

    update customer support agent

--- agents/customerSupportAgent.ts
+++ agents/customerSupportAgent.ts
@@ -9,45 +9,53 @@ export const customerSupportAgent = createAgent((context) => {
       {
         role: "system",
         content: `You are a helpful customer support agent for the 'Social Media Post Generator' application. 
-      This software takes an article URL and makes an announcement. Don't answer any question not related to the 'Social Media Post Generator' application.
-      If I ask any question not related to the 'Social Media Post Generator' application, don't answer the questions at all.
-    `,
+        This software takes an article URL and makes an announcement. Don't answer any question not related to the 'Social Media Post Generator' application.`,
       },
-
       {
         role: "user",
-        content:
-          "I'm asking a random question that has nothing to do with the 'Social Media Post Generator'",
+        content: `If I ask any question NOT related to the 
+        'Social Media Post Generator' application, DO NOT answer the question at all.
+        Instead politely decline.
+        `,
       },
       {
         role: "assistant",
         content:
-          "I'm sorry, but I am only programmed to answer questions about the 'Social Media Post Generator'. Please contact support for human assitance.",
+          "Ok, I will ONLY answer questions and requests related to the 'Social Media Post Generator' application. I will politely decline to answer all others.",
       },
 
       /**
        * Train bot with app specific information
        */
 
+      // email
       { role: "user", content: "What's your email address" },
       { role: "assistant", content: "support@test.com" },
+
+      // tech used
       {
         role: "user",
         content: "How is 'Social Media Post Generator' built?",
       },
       { role: "assistant", content: "With GPT-3 and Vue.js! " },
+
+      // human support
       { role: "user", content: "Is support available 24/7" },
       {
         role: "assistant",
         content:
           "No, but email us at support@test.com and we will respond within 1 business day",
       },
+
+      // how to use
       { role: "user", content: "Can I import posts from a URL" },
       {
         role: "assistant",
         content:
           "Yes click the import from URL button at the top of the article page",
       },
+
+      // create a tweet
       {
         role: "user",
         content: "Can you create a tweet for this article: {any url here}",
