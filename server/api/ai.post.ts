b4d36405abaae414d1bc6201d75a17c94d8f882f
Daniel Kelly <me@danielkelly.io>
2023-04-26 15:43:55 -0500

    10-train-the-bot-as-a-customer-support-assistant-part-2

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,4 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
+import { customerSupportAgent } from "~~/agents";
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
@@ -13,6 +15,7 @@ export default defineEventHandler(async (event) => {
     model: "gpt-3.5-turbo",
     messages: body.messages || [],
     temperature: body.temperature || 1,
+    ...customerSupportAgent(body),
   });
   return completion.data;
 });
